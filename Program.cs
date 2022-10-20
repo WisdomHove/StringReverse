4599a402603de43a41b6380721d14a475bf65acc
wisdomhove <hovewisdom@gmail.com>
2022-10-20 21:59:59 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StringReverse
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            string result =  "";
+
+            Console.WriteLine("Enter a string: ");
+            string str = Console.ReadLine();
+
+            for(int i = str.Length-1; i >= 0; i--){
+                result += str[i];
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
